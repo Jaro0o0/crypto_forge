a6137517a0e946995a9a5f4dd009f81668fbe0b7
Jar0 <jar0dev80@gmail.com>
2026-06-25 22:03:18 +0200

    save

--- src/components/ETH/ETH.jsx
+++ src/components/ETH/ETH.jsx
@@ -2,16 +2,15 @@
 
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
-import { Center } from "@react-three/drei";
-import { Logo } from "../Logo/Ethereum_logo";
+import { Center, useGLTF } from "@react-three/drei";
 
 function ETH() {
     const logoRef = useRef();
+    const { nodes, materials } = useGLTF('/models/ethereum_logo.glb');
 
-    // Animation: Rotate the logo
     useFrame((state, delta) => {
         if (logoRef.current) {
-            logoRef.current.rotation.y += delta * 0.5; // Adjust speed as needed
+            logoRef.current.rotation.y += delta * 0.5;
         }
     });
 
@@ -23,7 +22,34 @@ function ETH() {
             
             <Center>
                 <group ref={logoRef} scale={3.6}>
-                    <Logo />
+                    <group name="Sketchfab_Scene">
+                        <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, 0]} scale={1.107}>
+                            <group name="161ee2e266e94c7791940d78a5a83d04fbx" rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+                                <group name="Object_2">
+                                    <group name="RootNode">
+                                        <group name="eth" rotation={[-Math.PI, -1.556, -Math.PI]} scale={[1, 1, 1.145]}>
+                                            <group name="Pyramid_3" position={[0.217, 73.807, 0.929]} rotation={[0, Math.PI / 4, 0]}>
+                                                <mesh name="Pyramid_3_lambert3_0" geometry={nodes.Pyramid_3_lambert3_0.geometry} material={materials.lambert3} />
+                                                <mesh name="Pyramid_3_lambert4_0" geometry={nodes.Pyramid_3_lambert4_0.geometry} material={materials.lambert4} />
+                                                <mesh name="Pyramid_3_lambert2_0" geometry={nodes.Pyramid_3_lambert2_0.geometry} material={materials.lambert2} />
+                                            </group>
+                                            <group name="Pyramid_1" position={[-0.108, -36.302, 32.916]} rotation={[-Math.PI / 2, 0, -Math.PI]}>
+                                                <group name="transform1" />
+                                            </group>
+                                            <group name="Pyramid_2" position={[-0.108, -36.302, -33.845]} rotation={[-Math.PI / 2, 0, 0]}>
+                                                <group name="transform2" />
+                                            </group>
+                                        </group>
+                                        <group name="Pyramid">
+                                            <mesh name="Pyramid_Default_Material_0" geometry={nodes.Pyramid_Default_Material_0.geometry} material={materials.Default_Material} />
+                                            <mesh name="Pyramid_lambert4_0" geometry={nodes.Pyramid_lambert4_0.geometry} material={materials.lambert4} />
+                                            <mesh name="Pyramid_lambert3_0" geometry={nodes.Pyramid_lambert3_0.geometry} material={materials.lambert3} />
+                                        </group>
+                                    </group>
+                                </group>
+                            </group>
+                        </group>
+                    </group>
                 </group>
             </Center>
         </>
@@ -31,3 +57,4 @@ function ETH() {
 }
 
 export default ETH;
+
